59df8792dc8d50f1e36c7b505e3c2148ab40fec1
Ibrahim01 <164147442+Hima00l@users.noreply.github.com>
2025-12-12 16:36:34 +0200

    Add files via upload

---
+++ Problem-Solving/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PS
+{
+    internal class Program
+    {
+        public static string ShortWord(string[] Word)
+        {
+            string [] words = Word;
+
+            if (words == null ) 
+                throw new ArgumentNullException(nameof(words),"This list is Empty");
+
+
+            string shortest = words[0];
+            foreach (string word in words)
+            {
+                if(word.Length < shortest.Length)
+                    shortest = word;
+            }
+            return shortest;
+        }
+        static void Main(string[] args)
+        {
+            string[] word = { "Code", "Backend", "Circle", "Ai" };
+            Console.WriteLine(ShortWord(word));
+
+            
+        }
+    }
+}
+
